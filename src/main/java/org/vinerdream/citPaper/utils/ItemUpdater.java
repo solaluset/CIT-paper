6fbb20dba1a5edb72fdac4a2cfda7f4406d4252c
Sóla Lusøt <60041069+solaluset@users.noreply.github.com>
2025-01-03 19:15:19 +0200

    Update

--- src/main/java/org/vinerdream/citPaper/utils/ItemUpdater.java
+++ src/main/java/org/vinerdream/citPaper/utils/ItemUpdater.java
@@ -2,16 +2,20 @@ package org.vinerdream.citPaper.utils;
 
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
+import org.bukkit.NamespacedKey;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataType;
 import org.vinerdream.citPaper.CITPaper;
 import org.vinerdream.citPaper.converter.ParsedTextureProperties;
 
 public class ItemUpdater {
     private final CITPaper plugin;
+    private final NamespacedKey isManagedKey;
 
     public ItemUpdater(CITPaper plugin) {
         this.plugin = plugin;
+        this.isManagedKey = new NamespacedKey(plugin, "is_managed");
     }
 
     public void updateItem(ItemStack item) {
@@ -20,6 +24,7 @@ public class ItemUpdater {
     }
 
     public void updateItem(ItemStack item, String name) {
+        ItemMeta meta = item.getItemMeta();
         for (ParsedTextureProperties data : plugin.getRenames()) {
             if (data.getNamePattern() == null) continue;
             if (data.getItems().stream().noneMatch(itemKey -> item.getType().getKey().asString().equals(itemKey))) {
@@ -28,10 +33,16 @@ public class ItemUpdater {
             if (!data.getNamePattern().matcher(name).find()) {
                 continue;
             }
-            ItemMeta meta = item.getItemMeta();
             meta.setItemModel(data.getKey());
+            meta.getPersistentDataContainer().set(isManagedKey, PersistentDataType.BOOLEAN, true);
             item.setItemMeta(meta);
             return;
+        }
+
+        if (meta.getPersistentDataContainer().has(isManagedKey)) {
+            meta.setItemModel(null);
+            meta.getPersistentDataContainer().remove(isManagedKey);
+            item.setItemMeta(meta);
         }
     }
 }
