c7a63d0e0366cad11a706637fa1a10e174e5a952
Sóla Lusøt <60041069+solaluset@users.noreply.github.com>
2025-01-04 17:44:17 +0200

    Add unicode case-insensitivity

--- src/main/java/org/vinerdream/citPaper/utils/NameMatcher.java
+++ src/main/java/org/vinerdream/citPaper/utils/NameMatcher.java
@@ -4,6 +4,8 @@ import java.util.regex.Pattern;
 import java.util.regex.PatternSyntaxException;
 
 public class NameMatcher {
+    public static final int CASE_INSENSITIVE_FLAGS = Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE;
+
     public static Pattern filterToPattern(String filter) {
         if (filter == null) return null;
 
@@ -11,11 +13,11 @@ public class NameMatcher {
             if (filter.startsWith("pattern:")) {
                 return Pattern.compile(patternToRegex(removePrefix(filter, "pattern:")));
             } else if (filter.startsWith("ipattern:")) {
-                return Pattern.compile(patternToRegex(removePrefix(filter, "ipattern:")), Pattern.CASE_INSENSITIVE);
+                return Pattern.compile(patternToRegex(removePrefix(filter, "ipattern:")), CASE_INSENSITIVE_FLAGS);
             } else if (filter.startsWith("regex:")) {
                 return Pattern.compile(removePrefix(filter, "regex:"));
             } else if (filter.startsWith("iregex:")) {
-                return Pattern.compile(removePrefix(filter, "iregex:"), Pattern.CASE_INSENSITIVE);
+                return Pattern.compile(removePrefix(filter, "iregex:"), CASE_INSENSITIVE_FLAGS);
             }
             return Pattern.compile(Pattern.quote(filter));
         } catch (PatternSyntaxException e) {
