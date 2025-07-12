b4dd5584c3d1d0a2c3f4fa48430fd60e84372a25
Sóla Lusøt <60041069+solaluset@users.noreply.github.com>
2025-07-12 11:36:28 +0300

    Add file blacklist

--- src/main/java/org/vinerdream/citPaper/utils/ZipUtils.java
+++ src/main/java/org/vinerdream/citPaper/utils/ZipUtils.java
@@ -3,6 +3,8 @@ package org.vinerdream.citPaper.utils;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Locale;
+import java.util.Set;
 import java.util.stream.Stream;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
@@ -11,11 +13,16 @@ import java.util.zip.ZipOutputStream;
 import static org.vinerdream.citPaper.utils.CollectionUtils.iterateStream;
 
 public class ZipUtils {
+    private static final Set<String> FILENAME_BLACKLIST = Set.of("desktop.ini");
+
     public static void unzip(Path file, Path outputDirectory) throws IOException {
         try (ZipFile zip = new ZipFile(file.toFile())) {
             for (ZipEntry entry : iterateStream(zip.stream())) {
                 if (entry.isDirectory()) return;
                 Path extractedPath = outputDirectory.resolve(entry.getName());
+                if (isBlacklisted(extractedPath)) {
+                    continue;
+                }
                 extractedPath.getParent().toFile().mkdirs();
                 try (FileOutputStream stream = new FileOutputStream(extractedPath.toFile())) {
                     zip.getInputStream(entry).transferTo(stream);
@@ -34,6 +41,9 @@ public class ZipUtils {
                     for (Path filePath : iterateStream(files)) {
                         File file = filePath.toFile();
                         if (file.isDirectory()) return;
+                        if (isBlacklisted(filePath)) {
+                            continue;
+                        }
                         zip.putNextEntry(new ZipEntry(directory.relativize(filePath).toString().replace(File.separator, "/")));
                         byte[] data = new byte[1024];
                         try (FileInputStream reader = new FileInputStream(file)) {
@@ -47,5 +57,9 @@ public class ZipUtils {
                 }
             }
         }
+    }
+
+    private static boolean isBlacklisted(Path path) {
+        return FILENAME_BLACKLIST.contains(path.getFileName().toString().toLowerCase(Locale.ROOT));
     }
 }
