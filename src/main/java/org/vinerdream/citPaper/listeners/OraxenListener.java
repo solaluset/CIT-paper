6764e44a643c300950e8707faa4177b42a4cad66
Sóla Lusøt <60041069+solaluset@users.noreply.github.com>
2025-12-08 03:04:22 +0200

    Make base armor transparent

--- src/main/java/org/vinerdream/citPaper/listeners/OraxenListener.java
+++ src/main/java/org/vinerdream/citPaper/listeners/OraxenListener.java
@@ -1,9 +1,14 @@
 package org.vinerdream.citPaper.listeners;
 
 import io.th0rgal.oraxen.api.events.OraxenItemsLoadedEvent;
+import io.th0rgal.oraxen.api.events.OraxenPackGeneratedEvent;
+import io.th0rgal.oraxen.utils.VirtualFile;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.vinerdream.citPaper.CITPaper;
+
+import java.util.List;
+import java.util.Locale;
 
 public class OraxenListener implements Listener {
     private final CITPaper plugin;
@@ -15,5 +20,17 @@ public class OraxenListener implements Listener {
     @EventHandler
     public void onItemsLoaded(OraxenItemsLoadedEvent event) {
         plugin.loadRenames();
+    }
+
+    @EventHandler
+    public void onPackGenerated(OraxenPackGeneratedEvent event) {
+        final String armorType = plugin.getConfig().getString("oraxen.armorType", "CHAINMAIL").toLowerCase(Locale.ROOT);
+        for (String texture : List.of("1", "2")) {
+            event.getOutput().add(new VirtualFile(
+                    "assets/minecraft/textures/models/armor",
+                    String.format("%s_layer_%s.png", armorType, texture),
+                    getClass().getResourceAsStream("/images/armor_transparent.png")
+            ));
+        }
     }
 }
