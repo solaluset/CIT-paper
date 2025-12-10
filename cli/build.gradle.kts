15c32f87602dae01c7066b07322eafb3d17edd91
Sóla Lusøt <60041069+solaluset@users.noreply.github.com>
2025-12-10 04:44:22 +0200

    Bump version to 1.5.1-SNAPSHOT

--- cli/build.gradle.kts
+++ cli/build.gradle.kts
@@ -15,7 +15,7 @@ plugins {
 }
 
 group = "org.vinerdream"
-version = "1.5.0"
+version = "1.5.1-SNAPSHOT"
 
 repositories {
     mavenLocal()
@@ -26,7 +26,7 @@ repositories {
 
 val proguard: Configuration by configurations.creating
 
-val citPaper = "org.vinerdream:CIT-paper:1.5.0"
+val citPaper = "org.vinerdream:CIT-paper:1.5.1-SNAPSHOT"
 val paperAPI = "io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT"
 
 dependencies {
